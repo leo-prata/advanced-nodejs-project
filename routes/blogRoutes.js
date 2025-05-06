c7a92912a487841809dfdaa1927dd073f7feb1f0
Leonardo Prata <leonardo.prata@smartnx.com>
2025-05-06 08:02:21 -0300

    starting with redis

--- routes/blogRoutes.js
+++ routes/blogRoutes.js
@@ -3,36 +3,53 @@ const requireLogin = require('../middlewares/requireLogin');
 
 const Blog = mongoose.model('Blog');
 
-module.exports = app => {
-  app.get('/api/blogs/:id', requireLogin, async (req, res) => {
-    const blog = await Blog.findOne({
-      _user: req.user.id,
-      _id: req.params.id
-    });
+module.exports = (app) => {
+	app.get('/api/blogs/:id', requireLogin, async (req, res) => {
+		const blog = await Blog.findOne({
+			_user: req.user.id,
+			_id: req.params.id,
+		});
 
-    res.send(blog);
-  });
+		res.send(blog);
+	});
 
-  app.get('/api/blogs', requireLogin, async (req, res) => {
-    const blogs = await Blog.find({ _user: req.user.id });
+	app.get('/api/blogs', requireLogin, async (req, res) => {
+		const redis = require('redis');
+		const redisURL = 'redis://127.0.0.1:6379';
+		const client = redis.createClient(redisURL);
+		const util = require('util');
+		client.get = util.promisify(client.get);
 
-    res.send(blogs);
-  });
+		// do we have any cached information in redis?
+		const cached_blogs = await client.get(req.user.id);
 
-  app.post('/api/blogs', requireLogin, async (req, res) => {
-    const { title, content } = req.body;
+		// if yes, respond right away and return
+		if (cached_blogs) {
+			console.log('serving from cache');
+			return res.send(cached_blogs);
+		}
 
-    const blog = new Blog({
-      title,
-      content,
-      _user: req.user.id
-    });
+		//if no, respond the request and update the cache
+		const blogs = await Blog.find({ _user: req.user.id });
+		res.send(blogs);
 
-    try {
-      await blog.save();
-      res.send(blog);
-    } catch (err) {
-      res.send(400, err);
-    }
-  });
+		client.set(req.user.id, JSON.stringify(blogs));
+	});
+
+	app.post('/api/blogs', requireLogin, async (req, res) => {
+		const { title, content } = req.body;
+
+		const blog = new Blog({
+			title,
+			content,
+			_user: req.user.id,
+		});
+
+		try {
+			await blog.save();
+			res.send(blog);
+		} catch (err) {
+			res.send(400, err);
+		}
+	});
 };
