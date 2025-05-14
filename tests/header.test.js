cf3875503a80d32e1fe07c3a98d6a52a3ffde0c0
Leonardo Prata <leonardo.prata@smartnx.com>
2025-05-14 11:56:20 -0300

    start testing with jest

---
+++ tests/header.test.js
@@ -0,0 +1,44 @@
+const puppeteer = require('puppeteer');
+const sessionFactory = require('./factories/session-factory');
+const userFactory = require('./factories/user-factory');
+
+let browser, page;
+
+beforeEach(async () => {
+	browser = await puppeteer.launch({
+		headless: false,
+	});
+
+	page = await browser.newPage();
+	await page.goto('http://localhost:3000');
+});
+
+afterEach(async () => {
+	await browser.close();
+});
+
+test('The header have the correct text', async () => {
+	const text = await page.$eval('a.brand-logo', (el) => el.innerHTML);
+	expect(text).toEqual('Blogster');
+});
+
+test('Clicking login starts OAuth flow', async () => {
+	await page.click('.right a');
+
+	const url = await page.url();
+	expect(url).toMatch(/accounts\.google\.com/);
+});
+
+test('When signed in, shows logout button', async () => {
+	const user = await userFactory();
+	const { session, signature } = sessionFactory(user);
+
+	await page.setCookie({ name: 'session', value: session });
+	await page.setCookie({ name: 'session.sig', value: signature });
+	await page.goto('http://localhost:3000');
+
+	await page.waitFor('a[href="/auth/logout"]');
+
+	const text = await page.$eval('a[href="/auth/logout"]', (el) => el.innerHTML);
+	expect(text).toEqual('Logout');
+});
